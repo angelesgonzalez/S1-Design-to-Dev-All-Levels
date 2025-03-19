e2d1bba8084ceeee41020ede2c17fe0e34c2c1af
angelesgonzalez <delosangelesgonzalez@gmail.com>
2025-03-19 16:46:21 +0100

    Refactor the tab section and create JS code for its handling.

--- main.js
+++ main.js
@@ -16,3 +16,44 @@ closeMobileMenu.addEventListener("click", (event) => {
 	showOrHideElement(mobileMenu);
 });
 
+document.addEventListener("DOMContentLoaded", () => {
+	const tabContainer = document.querySelector(".tabs-container");
+	const tabButtons = document.querySelectorAll(".tab-button");
+	const tabPanels = document.querySelectorAll(".tab-panel");
+
+	tabButtons[0].classList.add("active");
+	tabPanels[0].classList.add("active");
+	tabPanels[0].hidden = false;
+
+	tabContainer.addEventListener("click", (event) => {
+		const button = event.target.closest(".tab-button");
+		if (!button) return;
+		activateTab(button);
+	});
+
+	const activateTab = (selectedButton) => {
+		const panelId = selectedButton.getAttribute("aria-controls");
+		const selectedPanel = document.getElementById(panelId);
+
+		tabPanels.forEach((panel) => {
+			panel.classList.remove("active");
+			panel.hidden = true;
+		});
+
+		tabButtons.forEach((btn) => {
+			btn.classList.remove("active");
+		});
+
+		selectedButton.classList.add("active");
+		selectedButton.setAttribute("aria-selected", "true");
+		selectedPanel.hidden = false;
+		selectedPanel.classList.add("active");
+
+		tabButtons.forEach((btn) =>
+			btn.setAttribute(
+				"aria-selected",
+				btn === selectedButton ? "true" : "false"
+			)
+		);
+	};
+});
